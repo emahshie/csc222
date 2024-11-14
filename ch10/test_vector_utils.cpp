af4f62e0c96658d6ba765e3e24494f0446ea68ce
Mahshie <ella@mahshie.net>
2024-11-14 08:07:02 -0500

    step 2

--- ch10/test_vector_utils.cpp
+++ ch10/test_vector_utils.cpp
@@ -5,9 +5,23 @@
 #include "num_vector_utils.h"
 using namespace std;
 
-TEST_CASE("Testing render_num_vector") {
+TEST_CASE("Test 1") {
     vector<int> nums = {1, 3, 7};
     string numstr = render_num_vector(nums);
     string expected = "1 3 7";
     CHECK(numstr == expected);
+}
+
+TEST_CASE("Test 2"){
+vector<int> nums = {1,2,3,4,5,5};
+string numstr = render_num_vector(nums);
+string expected = "1 2 3 4 5 5";
+CHECK(numstr == expected);
+}
+
+TEST_CASE("Test 3"){
+vector<int> nums = {};
+string numstr = render_num_vector(nums);
+string expected = "";
+CHECK(numstr == expected);
 }
