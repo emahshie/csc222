1215a8503f4becc6ea06086274b99ca4f158ab60
Mahshie <ella@mahshie.net>
2024-11-18 12:06:21 -0500

    fully done

--- ch11/Time.cpp
+++ ch11/Time.cpp
@@ -8,6 +8,7 @@ Time::Time(int h, int m, double s)
   hour = h; minute = m; second = s;
 }
 
+
 void Time::print() const
 {
     cout << hour << ":" << minute << ":" << second << endl;
@@ -41,4 +42,18 @@ bool Time::after(const Time &t2) const
     if (minute < t2.minute) return false;
 
     return (second > t2.second);
+}
+
+Time Time::add(const Time &t2) const
+{
+    return Time(convert_to_seconds() + t2.convert_to_seconds());
+}
+
+Time::Time(double secs)
+{
+    hour = int(secs / 3600.0);
+    secs -= hour * 3600.0;
+    minute = int(secs / 60.0);
+    secs -= minute * 60;
+    second = secs;
 }
--- ch11/main.cpp
+++ ch11/main.cpp
@@ -2,16 +2,20 @@
 #include "Time.h"
 using namespace std;
 
-int main(){
-    Time current_time = {9, 14, 30.0};
+int main()
+{
+    Time current_time(9, 14, 30.0);
     current_time.increment(500.0);
     current_time.print();
-    cout<<current_time.convert_to_seconds();
-    Time done_time = {9,45,0.0};
-    cout<<endl;
-    if (done_time.after(current_time)) {
-    cout << "The bread will be done after it starts." << endl;
-    return 0;
-}
+    cout << endl;
 
+    Time bread_time(3, 35, 0.0);
+    Time done_time = current_time.add(bread_time);
+    done_time.print();
+    cout << endl;
+
+    if (done_time.after(current_time)) {
+        cout << "The bread will be done after it starts." << endl;
+    }
+    return 0;
 }
