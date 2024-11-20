74db7277f528a3c358c0830c04f6480cc8b5e50b
Mahshie <ella@mahshie.net>
2024-11-20 12:25:23 -0500

    #5 part 1 fully functional

--- ch11/Point.cpp
+++ ch11/Point.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 Point::Point(){
-    x=0.0;
-    y=2.0;
-}
-
-Point::Point(double x, double y){
     x=0;
     y=0;
+}
+
+Point::Point(double one, double two){
+    x=one;
+    y=two;
 }
