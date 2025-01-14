57fada23899042ea1e03fb7d06777a8ccf086dd5
Mahshie <ella@mahshie.net>
2025-01-14 09:16:17 -0500

    working greater than

--- finalproj/src/BigInt.cpp
+++ finalproj/src/BigInt.cpp
@@ -31,6 +31,41 @@ bool BigInt::operator == (const BigInt &b) const{
     return false;
 }
 
-bool BigInt::operator > (const BigInt &b) const{
-    return false;
+bool BigInt::operator>(const BigInt& num1) const{
+    if(!negative && num1.negative){
+        return  true;
+    }else if(negative && !num1.negative){
+        return false;
+    }
+    if(!negative){
+    if(digits.length() > num1.digits.length()){
+        return true;
+        }else if(digits.length() < num1.digits.length()){
+            return false;
+        }
+    }else if(negative){
+        if(digits.length() < num1.digits.length()){
+            return true;
+        }else if(digits.length() > num1.digits.length()){
+            return false;
+        }
+    }
+    if(!negative){
+        for(int i = 0; i < digits.length(); i++){
+            if(digits[i] > num1.digits[i]){
+                return true;
+            }else if(digits[i] < num1.digits[i]){
+                return false;
+            }
+        }
+    }
+    if(negative){
+        for(int i = 0; i < digits.length(); i++){
+            if(digits[i] < num1.digits[i]){
+                return true;
+            }else if(digits[i] > num1.digits[i]){
+                return false;
+            }
+        }
+    }
 }
